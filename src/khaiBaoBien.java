db12d19f4620d6920f6feb67d56537258ea2adbe
nguyenha265 <nguyenha2651988@gmail.com>
2019-07-05 08:34:55 +0700

    bai tap

---
+++ src/khaiBaoBien.java
@@ -0,0 +1,16 @@
+public class khaiBaoBien {
+    public static void main(String[] args) {
+        byte so = 10;
+        float soThuc = 20.5f;
+        double sothuc1 = 200000.3131245f;
+        boolean dungSai = true;
+        char kyTu = 'X';
+        String chuoi = "tap viet java";
+        System.out.println("số nguyên = " + so);
+        System.out.println("số thực = " + soThuc);
+        System.out.println("số thực lớn = " + sothuc1);
+        System.out.println(" boolean = " + dungSai);
+        System.out.println("ky tu = " + kyTu);
+        System.out.println("chuoi = " + chuoi);
+    }
+}
